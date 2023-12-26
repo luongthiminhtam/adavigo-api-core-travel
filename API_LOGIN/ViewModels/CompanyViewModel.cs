e3d658b98a2d3ecd238e16109cc6c2cfe67915e4
luongthiminhtam <52973027+luongthiminhtam@users.noreply.github.com>
2023-12-26 15:02:02 +0000

    Add project files.

---
+++ API_LOGIN/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace API_LOGIN.ViewModels
+{
+    public class CompanyViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string domain { get; set; }
+    }
+}
---
+++ REPOSITORIES/Repositories/Login/UserCoreRepository.cs
@@ -0,0 +1,100 @@
+﻿using DAL.Login;
+using Entities.ConfigModels;
+using ENTITIES.ViewModels.User;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using REPOSITORIES.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace REPOSITORIES.Repositories.Login
+{
+    public class UserCoreRepository: IUserCoreRepository
+    {
+        private readonly UserCoreDAL userDAL;
+        private readonly IOptions<DataBaseConfig> dataBaseConfig;
+        public UserCoreRepository(IOptions<DataBaseConfig> _dataBaseConfig)
+        {
+            userDAL = new UserCoreDAL(_dataBaseConfig.Value.SqlServer.ConnectionStringUser);
+            dataBaseConfig = _dataBaseConfig;
+        }
+        public async Task<List<UserMasterViewModel>> getDetail(long user_id, string username, string password)
+        {
+            try
+            {
+                return await userDAL.getDetail(user_id, username, password);
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.InsertLogTelegram("checkAuthent, username = " + username + " - UserRepository: " + ex);
+                return null;
+            }
+        }
+        public async Task<UserMasterViewModel> checkAuthent(string username, string password)
+        {
+            try
+            {
+               return await userDAL.getAuthentUserInfo(username, password);
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.InsertLogTelegram("checkAuthent, username = " + username + " - UserRepository: " + ex);
+                return null;
+            }
+        }
+        public async Task<long> upsertUser(UserMasterViewModel model)
+        {
+            try
+            {
+                return await userDAL.upsertUser(model);
+
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.InsertLogTelegram("upsertUse, user = "+ JsonConvert.SerializeObject(model) + " - UserRepository: " + ex);
+                return -1;
+            }
+        }
+
+        public async Task<long> changePassword(string username, string password)
+        {
+            try
+            {
+                return await userDAL.changePassword( username,  password);
+
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.InsertLogTelegram("upsertUse, user = " + username + " - UserRepository: " + ex);
+                return -1;
+            }
+        }
+
+       
+
+
+
+        public async Task<long> updateActive2Fa(long user_id)
+        {
+            try
+            {
+                return await userDAL.updateActive2Fa(user_id);
+
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.InsertLogTelegram("updateActive2Fa, user_id = " + user_id + " - UserRepository: " + ex);
+                return -1;
+            }
+        }
+
+        
+    }
+}
