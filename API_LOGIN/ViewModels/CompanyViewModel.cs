af20451a4d734ddb91857940b511f05e6a77d4b8
Lê Văn Cường <52973027+luongthiminhtam@users.noreply.github.com>
2024-01-21 17:40:31 +0700

    Delete API_LOGIN directory

--- API_LOGIN/ViewModels/CompanyViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_LOGIN.ViewModels
-{
-    public class CompanyViewModel
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string domain { get; set; }
-    }
-}
